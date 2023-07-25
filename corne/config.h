42aafa3ca9d77242d2cb6227f53ab3fb0c9c4d96
chenzhao <chenzhao>
2023-07-25 14:22:35 +0800

    优化

--- corne/config.h
+++ corne/config.h
@@ -1,41 +1,25 @@
-/*
- * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
- * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
-
-/* RGB settings. */
-#define RGBLED_NUM 54
-#define RGBLED_SPLIT \
-    { 27, 27 }
 
 /* RGB matrix support. */
 #ifdef RGB_MATRIX_ENABLE
     #define SPLIT_TRANSPORT_MIRROR
-    #define RGB_MATRIX_LED_COUNT RGBLED_NUM
-    #define RGB_MATRIX_SPLIT RGBLED_SPLIT
+    #define RGB_MATRIX_LED_COUNT 54
+    #define RGB_MATRIX_SPLIT { 27, 27 }
     #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
     #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
     #define RGB_DISABLE_WHEN_USB_SUSPENDED
-
-    #define RGB_MATRIX_KEYPRESSES // reacts to keypresses 
+    
     #define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-    #define ENABLE_RGB_MATRIX_SPLASH
-    #define ENABLE_RGB_MATRIX_SOLID_SPLASH
+    #define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+    #define ENABLE_RGB_MATRIX_DUAL_BEACON
+    #define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+    #define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+    #define ENABLE_RGB_MATRIX_HUE_PENDULUM
+    
+
+    // #define RGB_MATRIX_KEYPRESSES // reacts to keypresses 
+    // #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+    // #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+    // #define ENABLE_RGB_MATRIX_SPLASH
+    // #define ENABLE_RGB_MATRIX_SOLID_SPLASH
 #endif
--- corne/keymaps/default/keymap.c
+++ corne/keymaps/default/keymap.c
@@ -1,20 +1,3 @@
-/*
- * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -58,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_DEL,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,
+        RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                       KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX,                       KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             KC_LCTL, XXXXXXX,  KC_SPC,     KC_ENT, XXXXXXX, KC_LALT
                                         //`--------------------------'  `--------------------------'
