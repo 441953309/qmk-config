cc766dac1af686c14d0ec5b8afbabfdaf1332c6f
Qoo <441953309@qq.com>
2023-12-09 23:28:47 +0800

    关闭及删除rgb及oled相关代码

--- sofle/rules.mk
+++ sofle/rules.mk
@@ -2,8 +2,8 @@
 #   change yes to no to disable
 #
 SPLIT_KEYBOARD = yes
-RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
-RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
+RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
+RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
 
 BOOTMAGIC_ENABLE = no     	# Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no       	# Mouse keys
@@ -16,8 +16,8 @@ RGB_MATRIX_ENABLE = no     	# Enable keyboard RGB matrix (do not use together wi
 AUDIO_ENABLE = no           # Audio output
 AUDIO_SUPPORTED = no        # Audio is not supported
 
-OLED_ENABLE = no
-OLED_DRIVER = ssd1306
+# OLED_ENABLE = no
+# OLED_DRIVER = ssd1306
 
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 LTO_ENABLE = yes
