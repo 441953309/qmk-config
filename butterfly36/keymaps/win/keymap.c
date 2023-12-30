51b9e455902cd994da3efc20d35686b0be2450e2
Qoo <441953309@qq.com>
2023-12-30 17:50:38 +0800

    优化butterfly按键

--- butterfly36/keymaps/win/keymap.c
+++ butterfly36/keymaps/win/keymap.c
@@ -5,11 +5,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|------------+--------------+--------------+--------------+----------|            |----------+--------------+--------------+--------------+--------|
                    KC_Q,          KC_W,          KC_E,          KC_R,      KC_T,                   KC_Y,          KC_U,          KC_I,          KC_O,    KC_P,
         //|------------+--------------+--------------+--------------+----------|            |----------+--------------+--------------+--------------+--------|
-             LT(1,KC_A),  LCTL_T(KC_S),  LALT_T(KC_D),  LSFT_T(KC_F),      KC_G,                   KC_H,  RSFT_T(KC_J),  RALT_T(KC_K),  RCTL_T(KC_L), KC_QUOT,
+             LT(1,KC_A),  LGUI_T(KC_S),  LALT_T(KC_D),  LSFT_T(KC_F),      KC_G,                   KC_H,  RSFT_T(KC_J),  RALT_T(KC_K),  RGUI_T(KC_L), KC_QUOT,
         //|------------+--------------+--------------+--------------+----------|            |----------+--------------+--------------+--------------+--------|
                    KC_Z,          KC_X,          KC_C,          KC_V,      KC_B,                   KC_N,          KC_M,       KC_COMM,        KC_DOT, KC_SLSH,
         //|------------+--------------+--------------+--------------+----------|            |----------+--------------+--------------+--------------+--------|
-                                       LGUI_T(KC_ESC),  LT(2,KC_TAB),    KC_SPC,                 KC_ENT,       KC_BSPC,RGUI_T(KC_GRV)
+                                       LCTL_T(KC_ESC),  LT(2,KC_TAB),    KC_SPC,                 KC_ENT,       KC_BSPC,RCTL_T(KC_GRV)
                                     //`----------------------------------------'            `----------------------------------------'
         ),
 
@@ -17,11 +17,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
             XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, KC_MPLY,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
-            XXXXXXX, KC_LCTL, KC_LALT, KC_LSFT, XXXXXXX,          KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,
+            XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, XXXXXXX,          KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
             XXXXXXX, XXXXXXX, XXXXXXX,   KC_F2,  KC_F12,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
-                              KC_LGUI, KC_BSPC,  KC_ENT,           KC_ENT,  KC_DEL, KC_RGUI
+                              KC_LCTL, KC_BSPC,  KC_ENT,           KC_ENT,  KC_DEL, KC_RCTL
                           //`--------------------------'        `--------------------------'
         ),
 
