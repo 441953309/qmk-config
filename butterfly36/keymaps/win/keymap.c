27a95f9bcab99ff7f06603a611e45a63f207b4fe
Qoo <441953309@qq.com>
2024-01-15 22:49:24 +0800

    调整数字键

--- butterfly36/keymaps/win/keymap.c
+++ butterfly36/keymaps/win/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
             XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, XXXXXXX,          KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, XXXXXXX,   KC_F2,  KC_F12,            KC_F8, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX,   KC_F8,   KC_F2,  KC_F12,            KC_F8, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
                               KC_LCTL, KC_BSPC,  KC_ENT,           KC_ENT,  KC_DEL, KC_RCTL
                           //`--------------------------'        `--------------------------'
@@ -27,11 +27,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x5_3(
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
-               KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_UNDS, KC_PLUS, KC_LBRC, KC_LCBR, KC_LPRN,
-        //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
-               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_MINS,  KC_EQL, KC_RBRC, KC_RCBR, KC_RPRN,
-        //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
             KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_SCLN, KC_COLN,
+        //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
+               KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_MINS,  KC_EQL, KC_LBRC, KC_LCBR, KC_LPRN,
+        //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
+               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_UNDS, KC_PLUS, KC_RBRC, KC_RCBR, KC_RPRN,
         //|--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------|
                               XXXXXXX, XXXXXXX, XXXXXXX,          KC_PIPE,   MO(3), KC_BSLS
                           //`--------------------------'        `--------------------------'
